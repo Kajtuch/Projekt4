08e65d54b49808995c5257eba70c37205b23e1d2
kajtek <kajtekkajtek2003@gmail.com>
2023-08-23 16:50:26 +0200

    ustawienie sfmla

---
+++ winda/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SFML/Graphics.hpp> 
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+using namespace std;
+
+int main() {
+	cout << "hello world\n";
+
+	system("pause");
+	return 0;
+}
