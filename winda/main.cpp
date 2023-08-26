924d7e1b1f2523f1532b38087ee6508d8b1d57e1
kajtek <kajtekkajtek2003@gmail.com>
2023-08-26 20:05:21 +0200

    tworzenie okna

--- winda/main.cpp
+++ winda/main.cpp
@@ -10,6 +10,15 @@ using namespace std;
 int main() {
 	cout << "hello world\n";
 
+	sf::Window okno;//tworzym nasze okno jako obiekt klasy Window z zakresu nazw sf
+
+	okno.create(sf::VideoMode(1600, 600, 32), "winda"); // tutaj powstaje nasze okno pierwszy argument funkcji to wymiary okna np. "1600, 600," 32 oznacza zakres kolorów. drugi argument to nazwa okna
+
+	while (okno.isOpen()) {// pêtla wykonuje siê dopóki okno jest otwarte
+
+
+		okno.display();//metoda odpowiedzialna za wyœwietlanie zmian.
+	}
 	system("pause");
 	return 0;
 }
